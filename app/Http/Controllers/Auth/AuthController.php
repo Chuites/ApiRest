20a2d9f60412afcc1bc99189f484926a0c143482
chuites <carlosehs.9319@gmail.com>
2023-03-29 00:12:34 -0600

    Metodo de Registro

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password),
+        ]);
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'user'=>$user,
+            'token'=>$token
+        ], 201);
+    }
 }
