b8fb8124dcd1adafd9240583e92100028aecedef
chuites <carlosehs.9319@gmail.com>
2023-03-29 00:50:16 -0600

    Metodo de Login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,26 +7,57 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use JWTAuth;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
     public function register(Request $request){
-        $this->validate($request,[
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required',
-            'password' => 'required',
+            'password' => 'required'
         ]);
 
-        $user = User::create([
-            'name'=> $request->name,
-            'email'=> $request->email,
-            'password'=> Hash::make($request->password),
-        ]);
-        $token = JWTAuth::fromUser($user);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->all());
+        }else{
+            $user = User::create([
+                'name'=> $request->name,
+                'email'=> $request->email,
+                'password'=> Hash::make($request->password),
+            ]);
+            $token = JWTAuth::fromUser($user);
 
-        return response()->json([
-            'user'=>$user,
-            'token'=>$token
-        ], 201);
+            return response()->json([
+                'user'=>$user,
+                'token'=>$token
+            ], 201);
+        }
+    }
+
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->all());
+        }else{
+            $credenciales = $request->only('email','password');
+            try{
+                if(!$token = JWTAuth::attempt($credenciales)){
+                    return response()->json([
+                        'error' => 'Credenciales Invalidas'
+                    ], 400);
+                }
+            }catch(JWTExcepcion $e){
+                return response()->json([
+                    'error' => 'Token Invalido'
+                ], 500);
+            }
+            return response()->json(compact('token'));
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::post('TestConectividad','App\Http\Controllers\CategoriaController@TestConectividad');
 Route::post('CrearCuenta','App\Http\Controllers\CategoriaController@CrearCuenta');
